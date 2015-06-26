5258080659f3f0346f5f3897f915ac7f9a695907
Aleksandr Dodatko <a.dodatko@healthjoy.com>
2015-06-26 14:19:32 +0300

    [framework] Added dynamic framework target

---
+++ CXAlertView-iOS/CXAlertView-iOS-dynamic/CXAlertView-iOS-dynamic.h
@@ -0,0 +1,19 @@
+//
+//  CXAlertView-iOS-dynamic.h
+//  CXAlertView-iOS-dynamic
+//
+//  Created by Oleksandr Dodatko on 6/26/15.
+//  Copyright (c) 2015 healthjoy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CXAlertView-iOS-dynamic.
+FOUNDATION_EXPORT double CXAlertView_iOS_dynamicVersionNumber;
+
+//! Project version string for CXAlertView-iOS-dynamic.
+FOUNDATION_EXPORT const unsigned char CXAlertView_iOS_dynamicVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CXAlertView_iOS_dynamic/PublicHeader.h>
+
+
